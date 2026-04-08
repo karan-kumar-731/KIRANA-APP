15cabddcdb5a78c9a4dde0756be943fa6ae31ec5
karan580968909-del <karan580968909@gmail.com>
2026-04-08 11:22:18 +0530

    final fix deployment + image upload

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/products', require('./routes/Products'));
+app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/admin', require('./routes/admin'));
 
